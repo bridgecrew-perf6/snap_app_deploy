89456c72a59966d2affcb9b6d4b8c7077fa844cf
tmcgregor1989 <tmcgregor1989@gmail.com>
2022-03-29 21:13:14 +0100

    heroku trial

--- server/server.js
+++ server/server.js
@@ -4,10 +4,12 @@ const MongoClient = require('mongodb').MongoClient
 const createRouter = require('./helpers/create_router')
 const cors = require('cors')
 
+const PORT = process.env.PORT || 8080;
+
 app.use(express.json());
 app.use(cors())
 
-MongoClient.connect('mongodb://0.0.0.0:27017', {useUnifiedTopology : true})
+MongoClient.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017', {useUnifiedTopology : true})
 .then((client) => {
   const db = client.db('scoreboard')
   const playersCollection = db.collection('players')
@@ -15,7 +17,10 @@ MongoClient.connect('mongodb://0.0.0.0:27017', {useUnifiedTopology : true})
   app.use('/api/players', playersRouter)
 })
 .catch(console.error)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+}
 
-app.listen(5000, function () {
+app.listen(PORT, function () {
   console.log(`Listening on port ${ this.address().port }`);
 });
